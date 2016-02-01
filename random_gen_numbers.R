798fe2f191b0cac862b953490be574de871b0fc0
Victor-Alfred <vicalf_1@yahoo.com>
2016-02-01 19:09:54 +0100

    new R script to generate numbers, deleted previous output file

---
+++ random_gen_numbers.R
@@ -0,0 +1,6 @@
+# A short R script to generate an unordered set of random numbers within
+# a specified range, and of a specified size (i.e. how many numbers?)
+# e.g to generate a set of unordered random numbers between 10 and 50
+
+seq(10, 50, length.out = 15)    # generates 15 numbers between 10 and 50 that are evenly 
+                                # spaced
